b35c57978924ad8e2a66eb0fda28e5efae04391c
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-08-04 19:20:59 +0200

    Make check.py API friendly

--- src/check_ocp_ver/check.py
+++ src/check_ocp_ver/check.py
@@ -1,20 +1,23 @@
 """Check current page against last saved page."""
 
-from typing import Dict, List, Tuple
+from typing import Dict, List, Union
 
 import requests
 from bs4 import BeautifulSoup
 from tinydb import Query, TinyDB, where
-from tinydb.table import Document
 from baseline import filter_latest_stable_and_accepted_releases
 
 
-def check(current_parsed_page: List[Dict[str, str]], baseline: TinyDB):
+def check(
+    current_parsed_page: List[Dict[str, str]], baseline: TinyDB
+) -> List[Dict[str, Union[str, bool]]]:
     """Change this to check different versions"""
-    check_ver("4.16", current_parsed_page, baseline)
-    check_ver("4.18", current_parsed_page, baseline)
-    check_ver("4.19", current_parsed_page, baseline)
-    check_ver("4.20", current_parsed_page, baseline)
+    response: List[Dict[str, Union[str, bool]]] = []
+    response.append(check_ver("4.16", current_parsed_page, baseline))
+    response.append(check_ver("4.18", current_parsed_page, baseline))
+    response.append(check_ver("4.19", current_parsed_page, baseline))
+    response.append(check_ver("4.20", current_parsed_page, baseline))
+    return response
 
 
 def save_and_acknowledge_version(
@@ -32,24 +35,35 @@ def update_and_acknowledge_version(current_ver: str, prev_ver: str, baseline: Ti
     print("updated!")
 
 
-def check_ver(version: str, current_page: List[Dict[str, str]], baseline: TinyDB):
+def check_ver(
+    version: str, current_page: List[Dict[str, str]], baseline: TinyDB
+) -> Dict[str, Union[str, bool]]:
     current_group = [r for r in current_page if r["Version Grouping"] == version]
     saved_group = baseline.search(where("Version Grouping") == version)
+    response: Dict[str, Union[str, bool]] = {}
     if not current_group:
-        print("version", version)
-        print("Not found in current page")
+        response["current_version"] = version
+        response["saved_version"] = version
+        response["status"] = "Not found in current page"
+        response["changed"] = False
     elif not saved_group:
-        print("version", version)
-        print("Not saved")
         new_current_ver: str = current_group[0]["Name"]
+        response["current_version"] = version
+        response["saved_version"] = new_current_ver
+        response["status"] = "Version of a new group saved"
+        response["changed"] = False
         save_and_acknowledge_version(new_current_ver, current_group, baseline)
     else:
         current_ver: str = current_group[0]["Name"]
         saved_ver: str = saved_group[0]["Name"]
+        response["current_version"] = current_ver
+        response["saved_version"] = saved_ver
+        response["status"] = "ok"
+        response["changed"] = False
         if current_ver != saved_ver:
             update_and_acknowledge_version(current_ver, saved_ver, baseline)
-        else:
-            print("Current version:", current_ver, "Saved version:", saved_ver)
+            response["changed"] = False
+    return response
 
 
 def load() -> TinyDB:
@@ -62,7 +76,7 @@ def main():
         parsed_page = BeautifulSoup(r.text, features="html.parser")
         data = filter_latest_stable_and_accepted_releases(parsed_page)
         db = load()
-        check(data, db)
+        print(check(data, db))
     else:
         print("status_code", r.status_code)
 
