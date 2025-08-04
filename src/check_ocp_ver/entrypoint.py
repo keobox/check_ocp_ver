cd6ad14f47ac9d3d14c29d85a42b46f7b0f6018b
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-08-04 19:39:23 +0200

    Add an entrypoint to the check service

---
+++ src/check_ocp_ver/entrypoint.py
@@ -0,0 +1,21 @@
+"""Entrypoint of the check Openshift version 'service'"""
+
+import os
+
+import baseline
+import check
+
+
+def main():
+    """If a version database file does not exists create one, otherwise just query the
+    Openshift release web page and compare against the past saved versions."""
+    db_dir = os.environ.get("CHECK_DB_DIR", ".")
+    db_file = os.environ.get("CHECK_DB_DIR", "stable_accepted_releases.json")
+    if os.path.exists(os.path.join(db_dir, db_file)):
+        check.main()
+    else:
+        baseline.main()
+
+
+if __name__ == "__main__":
+    main()
