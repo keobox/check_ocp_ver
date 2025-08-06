13b82d37edd4efe7af7918b461f3634feff1b367
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-08-06 10:29:19 +0200

    Fix a bug in env variables

--- src/check_ocp_ver/entrypoint.py
+++ src/check_ocp_ver/entrypoint.py
@@ -10,7 +10,7 @@ def main():
     """If a version database file does not exists create one, otherwise just query the
     Openshift release web page and compare against the past saved versions."""
     db_dir = os.environ.get("CHECK_DB_DIR", ".")
-    db_file = os.environ.get("CHECK_DB_DIR", "stable_accepted_releases.json")
+    db_file = os.environ.get("CHECK_DB_FILE", "stable_accepted_releases.json")
     if os.path.exists(os.path.join(db_dir, db_file)):
         check.main()
     else:
